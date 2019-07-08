9e7469d831b0e4f4a42fd0756f3b8a76e9be9227
alenskupnjak alenskupnjak <alenskupnjak@yahoo.com>
2019-07-08 08:34:31 +0200

    001-  instalacija Ang Materijala...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MeniComponent } from './meni/meni.component';
+import { ZadatakComponent } from './meni/zadatak/zadatak.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeniComponent,
+    ZadatakComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/meni/zadatak/zadatak.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-zadatak',
+  templateUrl: './zadatak.component.html',
+  styleUrls: ['./zadatak.component.scss']
+})
+export class ZadatakComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
