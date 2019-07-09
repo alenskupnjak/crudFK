c2bbb0f21c1dd30e71b3f039d9c42d7ad69b5606
alenskupnjak alenskupnjak <alenskupnjak@yahoo.com>
2019-07-09 13:29:45 +0200

    v1.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
   production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyD0DIB1YQLgR_Tuzix1-mIaeNTJcGjCDkk",
+    authDomain: "crudfk.firebaseapp.com",
+    databaseURL: "https://crudfk.firebaseio.com",
+    projectId: "crudfk",
+    storageBucket: "",
+    messagingSenderId: "562393219698",
+    appId: "1:562393219698:web:f47c4790af675700"
+  }
 };
